6745a783570a5bc528199d4ba6ec966fd76ffe97
Jonathan Jensen Jr <jonathanjensenjr@gmail.com>
2017-11-20 17:02:48 -0700

    asdf

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ var salt = bcrypt.genSaltSync(10);
 const cookieSession = require('cookie-session')
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")//for deployment
+  res.header("Access-Control-Allow-Origin", "http://localhost:3131")//for deployment
   // res.header("Access-Control-Allow-Origin", "http://localhost:3001")//for running locally
 
   res.header("Access-Control-Allow-Credentials", "true")
