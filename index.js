0ef4704c251602d8ca754f97cbbbdaaa6ddbd816
Jonathan Jensen Jr <jonathanjensenjr@gmail.com>
2017-12-15 16:12:54 -0700

    asdf

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ var salt = bcrypt.genSaltSync(10);
 const cookieSession = require('cookie-session')
 
 app.use(function(req, res, next) {
-  // res.header("Access-Control-Allow-Origin", "https://infinite-hollows-12472.herokuapp.com")//for heroku
-  res.header("Access-Control-Allow-Origin", "http://localhost:3132")//for running locally
+  res.header("Access-Control-Allow-Origin", "https://infinite-hollows-12472.herokuapp.com")//for heroku
+  // res.header("Access-Control-Allow-Origin", "http://localhost:3132")//for running locally
   res.header("Access-Control-Allow-Credentials", "true")
   res.header("Access-Control-Allow-Methods", "GET,POST,DELETE,PATCH,PUT")
   res.header("Access-Control-Allow-Headers", "Content-Type, Accept")
