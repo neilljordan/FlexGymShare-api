a30ef83b89e18ef2608a205f05cbd85df6d5026c
Jack <jacktbader5@gmail.com>
2018-01-17 14:56:18 -0700

    re-pushing

--- routes/ammenities.js
+++ routes/ammenities.js
@@ -4,6 +4,8 @@ const express = require('express');
 const knex = require('../knex');
 const router = express.Router();
 
+//
+
 router.get('/ammenities', (req, res, next) => {
   knex('ammenities')
   .orderBy('id')
