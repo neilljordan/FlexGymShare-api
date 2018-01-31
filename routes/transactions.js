2ee7cb05c79508856901c16ff15857b41a7871d9
Matthew Webster <mattyweb@gmail.com>
2018-01-31 14:09:52 -0700

    fixed problems with daypasses and transactions not including pass type names

--- routes/transactions.js
+++ routes/transactions.js
@@ -6,10 +6,10 @@ const router = express.Router();
 const salt = bcrypt.genSaltSync(10);
 
 router.get('/transactions', (req, res, next) => {
-  knex('transaction')
-    .select('*')
-    .column(knex.raw('(select pass_type.name from pass_type, transaction where pass_type.id = transaction.pass_type_id) as pass_name'))
-    .orderBy('id')
+  knex.select('*')
+    .from('transaction')
+    .innerJoin('pass_type', 'pass_type.id', 'transaction.pass_type_id')
+    .orderBy('transaction.id')
     .then((transactions) => {
       res.json(transactions);
     })
@@ -20,10 +20,10 @@ router.get('/transactions', (req, res, next) => {
 
 router.get('/transactions/user/:id', (req, res, next) => {
   const userId = req.params.id;
-  knex('transaction')
-    .select('*')
-    .column(knex.raw('(select pass_type.name from pass_type, transaction where pass_type.id = transaction.pass_type_id) as pass_name'))
-    .orderBy('id')
+  knex.select('*')
+    .from('transaction')
+    .innerJoin('pass_type', 'pass_type.id', 'transaction.pass_type_id')
+    .orderBy('transaction.id')
     .where('user_id', userId)
     .then((transactions) => {
       res.json(transactions);
@@ -35,10 +35,11 @@ router.get('/transactions/user/:id', (req, res, next) => {
 
 router.get('/transactions/:id', (req, res, next) => {
   const transactionId = req.params.id;
-  knex('transaction')
-    .select('*')
-    .column(knex.raw('(select pass_type.name from pass_type, transaction where pass_type.id = transaction.pass_type_id) as pass_name'))
-    .where('id', transactionId)
+  knex.select('*')
+    .from('transaction')
+    .innerJoin('pass_type', 'pass_type.id', 'transaction.pass_type_id')
+    .orderBy('transaction.id')
+    .where('transaction.id', transactionId)
     .then((transactions) => {
       res.json(transactions[0]);
     })
@@ -50,6 +51,9 @@ router.post('/transactions', (req, res, next) => {
     pass_date, pass_type_id, user_id, listing_id, hash, gym_id, currentTime,
   } = req.body;
   const pass = currentTime + pass_date + user_id + listing_id + gym_id + pass_type_id;
+  const crypto = require('crypto');
+  let id = crypto.randomBytes(20).toString('hex');
+
   knex('transaction')
     .insert({
       pass_date,
