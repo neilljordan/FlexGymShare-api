48ac6caa936823aa1ab3a03443b1ba523c0f48a2
Matthew Webster <mattyweb@gmail.com>
2018-02-09 10:46:40 -0700

    updated transaction route to daypass

--- routes/qr.js
+++ routes/qr.js
@@ -7,18 +7,18 @@ const router = express.Router();
 // TODO: change the route name from /qrCodes to something like /passes
 // generate a QR PNG with embedded URL based on a code
 router.get('/qrCodes/:code', (req, res) => {
-  const transactionCode = req.params.code;
-  const pngString = qr.imageSync(`${process.env.ORIGIN_HOST}/verification/${transactionCode}`, { type: 'png' });
+  const passCode = req.params.code;
+  const pngString = qr.imageSync(`${process.env.ORIGIN_HOST}/verification/${passCode}`, { type: 'png' });
   res.send(pngString);
 });
 
 // find the transaction record associated with the code
 router.get('/qrCodes/verification/:code', (req, res) => {
-  const transactionCode = req.params.code;
-  knex('transaction')
-    .where('code', transactionCode)
-    .then((newTransaction) => {
-      res.json(newTransaction[0]);
+  const passCode = req.params.code;
+  knex('daypass')
+    .where('code', passCode)
+    .then((rows) => {
+      res.json(rows[0]);
     })
     .catch(err => next(err));
 });
