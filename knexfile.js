b9f0900116eaab489e63bbcfcd4751f42e334bd9
Matthew Webster <mattyweb@gmail.com>
2018-02-19 14:58:30 -0700

    again

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
+    connection: `${process.env.DATABASE_URL}?ssl=true`,
     ssl: true,
   },
 };
