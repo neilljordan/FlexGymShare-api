72084bae1a2249572b157fb07ba92aa9b94da1cd
Matthew Webster <mattyweb@gmail.com>
2018-02-19 14:51:34 -0700

    trying again

--- knex.js
+++ knex.js
@@ -1,8 +1,9 @@
 const environment = process.env.NODE_ENV || 'development';
-const knexConfig = require('./knexfile')[environment];
 const pg = require('pg');
 
 if (environment !== 'development') pg.defaults.ssl = true;
+
+const knexConfig = require('./knexfile')[environment];
 const knex = require('knex')(knexConfig);
 
 module.exports = knex;
