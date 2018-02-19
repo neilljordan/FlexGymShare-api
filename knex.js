aff29052cd924c7f6d2fc6bbec492cc972a55d0a
Matthew Webster <mattyweb@gmail.com>
2018-02-19 14:39:22 -0700

    trying another way to use SSL with Postgres

--- knex.js
+++ knex.js
@@ -1,5 +1,8 @@
 const environment = process.env.NODE_ENV || 'development';
 const knexConfig = require('./knexfile')[environment];
+const pg = require('pg');
+
+if (environment !== 'development') pg.defaults.ssl = true;
 const knex = require('knex')(knexConfig);
 
 module.exports = knex;
